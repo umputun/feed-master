ffcab057d549122adab9e47fdc95a3938bebf4e5
Eugene <eugene@umputun.com>
2019-02-22 16:11:26 -0600

    restore images controller

--- app/api/server.go
+++ app/api/server.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/didip/tollbooth"
@@ -19,6 +22,7 @@ import (
 	"github.com/umputun/feed-master/app/proc"
 )
 
+// Server provides HTTP API
 type Server struct {
 	Version    string
 	Conf       proc.Conf
@@ -26,6 +30,7 @@ type Server struct {
 	httpServer *http.Server
 }
 
+// Run starts http server for API with all routes
 func (s *Server) Run(port int) {
 
 	router := chi.NewRouter()
@@ -45,6 +50,7 @@ func (s *Server) Run(port int) {
 
 	router.Get("/rss/{name}", s.getFeedCtrl)
 	router.Get("/list", s.getListCtrl)
+	router.Get("/image/{name}", s.getImageCtrl)
 
 	err := s.httpServer.ListenAndServe()
 	log.Printf("[WARN] http server terminated, %s", err)
@@ -77,6 +83,32 @@ func (s *Server) getFeedCtrl(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/xml; charset=UTF-8")
 	fmt.Fprintf(w, "%s", string(b))
+}
+
+// GET /image/{name}
+func (s *Server) getImageCtrl(w http.ResponseWriter, r *http.Request) {
+
+	http.HandleFunc("/image/", func(w http.ResponseWriter, r *http.Request) {
+		fm := chi.URLParam(r, "name")
+		fm = strings.TrimRight(fm, ".png")
+		feedConf, found := s.Conf.Feeds[fm]
+		if !found {
+			rest.SendErrorJSON(w, r, log.Default(), http.StatusBadRequest,
+				errors.New("image "+chi.URLParam(r, "name")+" not found"), "failed to load image")
+			return
+		}
+
+		b, err := ioutil.ReadFile(feedConf.Image)
+		if err != nil {
+			rest.SendErrorJSON(w, r, log.Default(), http.StatusBadRequest,
+				errors.New("can't read  "+chi.URLParam(r, "name")), "failed to read image")
+			return
+		}
+		w.Header().Set("Content-Type", "image/png")
+		if _, err := w.Write(b); err != nil {
+			log.Printf("[WARN] failed to send image, %s", err)
+		}
+	})
 }
 
 // GET /list - returns feed's image
