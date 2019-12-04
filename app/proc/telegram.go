c2f5de1ce5e4632552b117973ed4598b5e59a126
Sergey Gaynetdinov <sergey@gaynetdinov.ru>
2019-12-05 01:48:12 +0500

    Send audio if file size less 50 Mb or send only text

--- app/proc/telegram.go
+++ app/proc/telegram.go
@@ -1,7 +1,10 @@
 package proc
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -26,7 +29,7 @@ func NewTelegramClient(token string) (*TelegramClient, error) {
 
 	bot, err := tb.NewBot(tb.Settings{
 		Token:  token,
-		Client: &http.Client{Timeout: 10 * time.Second},
+		Client: &http.Client{Timeout: 60 * 10 * time.Second},
 	})
 	if err != nil {
 		return nil, err
@@ -48,18 +51,97 @@ func (client TelegramClient) Send(channelID string, item feed.Item) error {
 		return nil
 	}
 
-	message, err := client.Bot.Send(
-		recipient{chatID: channelID},
-		client.getMessageHTML(item),
-		tb.ModeHTML,
-		tb.NoPreview,
-	)
+	contentLength, err := getContentLength(item.Enclosure.URL)
+	if err != nil {
+		return err
+	}
+
+	var message *tb.Message
+
+	if contentLength < 50_000_000 {
+		message, err = client.sendAudio(channelID, item)
+	} else {
+		message, err = client.sendText(channelID, item)
+	}
+
 	if err != nil {
 		return err
 	}
 
 	log.Printf("[DEBUG] send telegram message: \n%s", message.Text)
 	return err
+}
+
+func getContentLength(url string) (int64, error) {
+	resp, err := http.Head(url) //nolint:gosec
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return 0, errors.New("status code != 200")
+	}
+
+	log.Printf("[DEBUG] Content-Length: %d, url: %s", resp.ContentLength, url)
+	return resp.ContentLength, err
+}
+
+func (client TelegramClient) sendText(channelID string, item feed.Item) (*tb.Message, error) {
+	message, err := client.Bot.Send(
+		recipient{chatID: channelID},
+		client.getMessageHTML(item),
+		tb.ModeHTML,
+		tb.NoPreview,
+	)
+
+	return message, err
+}
+
+func (client TelegramClient) sendAudio(channelID string, item feed.Item) (*tb.Message, error) {
+	file, err := client.downloadAudio(item.Enclosure.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	audio := tb.Audio{
+		File:     tb.FromReader(bytes.NewReader(*file)),
+		FileName: "1.mp3",
+		MIME:     "audio/mpeg",
+		Caption:  client.getMessageHTML(item),
+		Title:    item.Title,
+	}
+
+	message, err := audio.Send(
+		client.Bot,
+		recipient{chatID: channelID},
+		&tb.SendOptions{
+			ParseMode: tb.ModeHTML,
+		},
+	)
+
+	return message, err
+}
+
+func (client TelegramClient) downloadAudio(url string) (*[]byte, error) {
+	clientHTTP := &http.Client{Timeout: 60 * 10 * time.Second}
+
+	resp, err := clientHTTP.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	log.Printf("[DEBUG] start download audio: %s", url)
+
+	file, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("[DEBUG] finish download audio: %s", url)
+	return &file, err
 }
 
 // https://core.telegram.org/bots/api#html-style
