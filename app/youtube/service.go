2ce8e37a63f9c4fdf41b8f05b4f544df0328e078
Umputun <umputun@gmail.com>
2022-03-29 11:35:39 -0500

    fix the prev commit, invert the check

--- app/youtube/service.go
+++ app/youtube/service.go
@@ -199,7 +199,7 @@ func (s *Service) procChannels(ctx context.Context) error {
 			processed++
 			log.Printf("[INFO] downloaded %s (%s) to %s, channel: %+v", entry.VideoID, entry.Title, file, feedInfo)
 			entry.File = file
-			if strings.Contains(entry.Title, feedInfo.Name) {
+			if !strings.Contains(entry.Title, feedInfo.Name) { // if title doesn't contains channel name add it
 				entry.Title = feedInfo.Name + ": " + entry.Title
 			}
 			ok, saveErr := s.Store.Save(entry)
