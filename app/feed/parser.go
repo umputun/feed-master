df4fb90c5763f9b3996bf34f2cfa4ec62a2f7a08
Umputun <umputun@gmail.com>
2022-04-01 19:26:02 -0500

    revert

--- app/feed/parser.go
+++ app/feed/parser.go
@@ -135,7 +135,6 @@ func parseAtom(content []byte) (Rss2, error) {
 }
 
 func parseFeedContent(content []byte) (Rss2, error) {
-	log.Printf("!! %s", string(content))
 	v := Rss2{}
 	err := xml.Unmarshal(content, &v)
 	if err != nil {
